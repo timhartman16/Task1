0ea4877b9d52129a2d0cbd4c8a7462a6cb2ac0ad
timhartman16 <djonatanas@yandex.ru>
2022-09-26 15:57:33 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		try
+		{
+			FailProcess();
+		}
+		catch { }
+
+		Console.WriteLine("Failed to fail process!");
+		Console.ReadKey();
+	}
+
+	static void FailProcess()
+	{
+		/*Вариант 1.
+		Завершаем этот процесс и возвращаем код выхода операционной системе.*/
+		Environment.Exit(0);
+
+		/*Вариант 2.
+		Принудительно завершаем процесс.*/
+		//process.Kill();
+		//process.WaitForExit(); //Дает компоненту Process команду ожидать завершения
+							   //связанного процесса в течение неограниченного времени.
+	}
+}
